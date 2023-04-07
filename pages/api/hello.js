af02e223b893290c9a1d80ba1486bee515ccbdb2
Yesukhei <yesukhei.t@unitel.mn>
2023-04-07 14:15:17 +0800

    last

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -64,6 +64,7 @@ export default function handler(req, res) {
         console.error(error);
       });
   };
-  setInterval(discover, 5000);
+  discover;
+  //setInterval(discover, 5000);
   res.status(200).json({ name: "John Doe" });
 }
