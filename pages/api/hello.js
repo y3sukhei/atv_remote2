f4982f9417214bc9f024ded14147f8aa066e7664
Yesukhei <yesukhei.t@unitel.mn>
2023-04-10 16:07:10 +0800

    try again

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -66,8 +66,8 @@ export default function (req, res) {
             //   console.debug("Pairing connected");
             // });
           } else {
-            //res.status(201).json({ status: "try again" });
-            discover();
+            res.status(201).json({ status: "try again" });
+            // discover();
           }
         })
         .catch((error) => {
