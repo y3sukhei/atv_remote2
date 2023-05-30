757d41f1aec17f66b3d358497710a2510390943a
Yesukhei <yesukhei.t@unitel.mn>
2023-05-30 16:41:42 +0800

    changes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,78 +2,127 @@
 import tls from "tls";
 import forge from "node-forge";
 import crypto from "crypto";
+import { AndroidRemote } from "androidtv-remote";
+import { Server } from "socket.io";
+import readLine from "readline";
+// const readLine = require("readline");
 const mDnsSd = require("node-dns-sd");
+const fs = require("fs");
+const path = require("path");
 
-export default function (req, res) {
-  const generateFull = (name, country, state, locality, organization, OU) => {
-    let keys = forge.pki.rsa.generateKeyPair(2048);
-    let cert = forge.pki.createCertificate();
-    cert.publicKey = keys.publicKey;
-    cert.serialNumber = "01" + crypto.randomBytes(19).toString("hex");
-    cert.validity.notBefore = new Date();
-    let date = new Date();
-    date.setUTCFullYear(2099);
-    cert.validity.notAfter = date;
-    let attributes = [
-      { name: "commonName", value: name },
-      { name: "countryName", value: country },
-      { shortName: "ST", value: state },
-      { name: "localityName", value: locality },
-      { name: "organizationName", value: organization },
-      { shortName: "OU", value: OU },
-    ];
-    cert.setSubject(attributes);
-    cert.sign(keys.privateKey, forge.md.sha256.create());
-
-    return {
-      cert: forge.pki.certificateToPem(cert),
-      key: forge.pki.privateKeyToPem(keys.privateKey),
-    };
-  };
-  //while (deviceList.length > 0) {
-  console.log("discover listening ");
-
-  return new Promise((resolve, reject) => {
-    discover();
-    async function discover() {
-      await mDnsSd
-        .discover({
-          name: "_services._dns-sd._udp.local",
-          quick: true,
-        })
-        .then((device_list) => {
-          console.log("device list ", JSON.stringify(device_list, null, "  "));
-
-          if (device_list.length > 0) {
-            console.log("device found ", JSON.stringify(device_list, null, "  "));
-
-            res.status(200).json(JSON.stringify(device_list, null, "  "));
-
-            resolve();
-            // return JSON.stringify(device_list, null, "  ");
-            // let options = {
-            //   key: generateFull("yesukhei", "Mongolia", "Ulaanbaatar", "Mongolia", "Univision", "idk").key,
-
-            //   //fs.readFileSync(path.resolve(__dirname,'../../cert/key.pem'))
-            //   cert: generateFull("yesukhei", "Mongolia", "Ulaanbaatar", "Mongolia", "Univision", "idk").cert,
-            //   //fs.readFileSync(path.resolve(__dirname,'../../cert/cert.pem'))
-            //   port: 6467,
-            //   host: "192.168.10.175",
-            //   rejectUnauthorized: false,
-            // };
-
-            // tls.connect(options, () => {
-            //   console.debug("Pairing connected");
-            // });
-          } else {
-            res.status(201).json({ status: "try again" });
-            // discover();
-          }
-        })
-        .catch((error) => {
-          console.error(error);
+const SocketHandler = (req, res) => {
+    const rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    if (req.method === "POST") {
+        let options = {
+            pairing_port: 6467,
+            remote_port: 6466,
+            name: "androidtv-remote",
+            cert: {},
+        };
+        // let androidRemote2 = new AndroidRemote("192.168.1.37", options);
+        const { body } = req;
+        console.log("body :", body);
+        androidRemote.sendCode(body.secret);
+        return res.status(200).json({ name: "paired" });
+    } else {
+        const { deviceId } = req.query;
+        console.log("device id :", deviceId);
+        let options = {
+            pairing_port: 6467,
+            remote_port: 6466,
+            name: "androidtv-remote",
+            cert: {},
+        };
+        let androidRemote = new AndroidRemote(deviceId, options);
+        // start pairing request
+        androidRemote.start();
+        // rl.question("Code : ", (code) => {
+        //     console.log(" asas" + code);
+        //     androidRemote.sendCode(code);
+        // });
+        androidRemote.on("secret", () => {
+            rl.question("Code : ", (code) => {
+                console.log(" " + code);
+                androidRemote.sendCode(code);
+            });
         });
+
+        // return res.status(200).json({ name: "connected" });
     }
+    // send code when ask to pair
+    // androidRemote.sendCode();
+    // let options = {
+    //     key:
+    //         // generateFull("yesukhei", "Mongolia", "Ulaanbaatar", "Mongolia", "Univision", "idk").key,
+    //         fs.readFileSync(path.resolve("./pages/cert/key.pem")),
+    //     cert:
+    //         //generateFull("yesukhei", "Mongolia", "Ulaanbaatar", "Mongolia", "Univision", "idk").cert,
+    //         fs.readFileSync(path.resolve("./pages/cert/cert.pem")),
+    //     port: 6467,
+    //     host: deviceId,
+    //     rejectUnauthorized: false,
+    // };
+    // const client = tls.connect(options, () => {
+    //     console.debug(options.host + " Pairing connected");
+    // });
+    // client.on("secureConnect", () => {
+    //     console.debug(options.host + " Pairing secure connected");
+    //     // pass pairing message
+    //     client.write("hello");
+    // });
+    // client.on("data", (data) => {
+    //     console.log("Received data :", data);
+    // });
+    // async function discover() {
+    //     await mDnsSd
+    //         .discover({
+    //             name: "_androidtvremote2._tcp.local.",
+    //             quick: true,
+    //         })
+    //         .then((device_list) => {
+    //             console.log("device list ", JSON.stringify(device_list, null, "  "));
+    //             if (device_list.length > 0) {
+    //                 console.log("device found ", JSON.stringify(device_list, null, "  "));
+    //                 res.status(200).json(JSON.stringify(device_list, null, "  "));
+    //                 // return JSON.stringify(device_list, null, "  ");
+    //                 let options = {
+    //                     key: generateFull(
+    //                         "yesukhei",
+    //                         "Mongolia",
+    //                         "Ulaanbaatar",
+    //                         "Mongolia",
+    //                         "Univision",
+    //                         "idk"
+    //                     ).key,
+    //                     //fs.readFileSync(path.resolve(__dirname,'../../cert/key.pem'))
+    //                     cert: generateFull(
+    //                         "yesukhei",
+    //                         "Mongolia",
+    //                         "Ulaanbaatar",
+    //                         "Mongolia",
+    //                         "Univision",
+    //                         "idk"
+    //                     ).cert,
+    //                     //fs.readFileSync(path.resolve(__dirname,'../../cert/cert.pem'))
+    //                     port: 6467,
+    //                     host: "192.168.10.175",
+    //                     rejectUnauthorized: false,
+    //                 };
+    //                 tls.connect(options, () => {
+    //                     console.debug("Pairing connected");
+    //                 });
+    //             } else {
+    //                 res.status(201).json({ status: "try again" });
+    //                 // discover();
+    //             }
+    //         })
+    //         .catch((error) => {
+    //             console.error(error);
+    //         });
+    // }
     // discover().then((result) => {
     //   if (result) {
     //     console.log("result :", result);
@@ -84,9 +133,6 @@ export default function (req, res) {
     //     console.log("error");
     //   }
     // });
-  });
-
-  //setInterval(discover, 5000);
-
-  // return res.status(200).json({ name: "hello" });
-}
+    //setInterval(discover, 5000);
+};
+export default SocketHandler;
--- pages/test/index.js
+++ pages/test/index.js
@@ -1,45 +1,334 @@
 import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import styles from "./index.module.css";
 
+let socket;
 const Test = () => {
-  const [devices, setDevices] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [text, setText] = useState("");
+    const [devices, setDevices] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [text, setText] = useState("");
+    const [inputValue, setInputValue] = useState("192.168.10.175");
+    const [secretValue, setSecretValue] = useState("");
+    const [message, setMessage] = useState([]);
 
-  useEffect(() => {
-    // logJSONData();
-  }, []);
+    useEffect(() => {
+        socket = io("http://localhost:8080");
 
-  async function logJSONData() {
-    const response = await fetch("/api/hello");
-    await response?.json().then((response) => {
-      if (response.status) {
-        setText(response.status);
-      }
-      if (response.length > 0) {
-        setDevices(response);
-        setLoading(false);
-        console.log("devices :", devices);
-      }
-      console.log(response);
-    });
-  }
+        // socket.connect("http://127.0.0.1:8080/pair");
+        socket.on("connect", () => {
+            console.log("Successfully connected");
+        });
 
-  return (
-    <>
-      <button
-        onClick={() => {
-          setText("");
-          setDevices("");
-          // console.log("devices :", devices);
-          logJSONData();
-        }}>
-        FETCH
-      </button>
-      {devices[0]?.fqdn}
-      {text}
-      <div>Enter</div>
-    </>
-  );
+        // socketInitializer();
+    }, []);
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSecretChange = (event) => {
+        setSecretValue(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // pair device
+        pairDevice();
+
+        // Handle form submission here
+        console.log("Submitted value:", inputValue);
+    };
+
+    const handleSecretSubmit = (event) => {
+        event.preventDefault();
+
+        // pair device
+        pairSecretValue();
+
+        // Handle form submission here
+        console.log("Submitted value:", inputValue);
+    };
+
+    async function pairSecretValue() {
+        socket.emit("secret", secretValue);
+    }
+
+    const sendMessage = (command) => {
+        // Here, you can implement the logic to send the command to the TV
+        console.log(`Sending command: ${command}`);
+        setMessage(`Sending command: ${command}`);
+    };
+
+    async function pairDevice() {
+        socket.emit("device id", inputValue);
+    }
+
+    const handleKeyPress = (event) => {
+        switch (event) {
+            case "up":
+                console.log("up");
+                socket.emit("key event", "up");
+                break;
+            case "down":
+                console.log("down");
+                socket.emit("key event", "down");
+                break;
+            case "left":
+                console.log("left");
+                socket.emit("key event", "left");
+                break;
+            case "right":
+                console.log("right");
+                socket.emit("key event", "right");
+                break;
+            case "ok":
+                console.log("enter");
+                socket.emit("key event", "ok");
+                break;
+            case "back":
+                console.log("back");
+                socket.emit("key event", "back");
+                break;
+            default:
+                console.log(
+                    "You need to add some cases here, a key you pressed was",
+                    event.keyCode
+                );
+        }
+    };
+
+    return (
+        <>
+            {/* <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Enter IP"
+                    value={inputValue}
+                    onChange={handleChange}
+                />
+                <button type="submit">Pair</button>
+            </form>
+
+            <form onSubmit={handleSecretSubmit}>
+                <input
+                    type="text"
+                    placeholder="Enter Pairing Code"
+                    value={secretValue}
+                    onChange={handleSecretChange}
+                />
+                <button type="submit">Enter</button>
+            </form> */}
+
+            <main>
+                <div class="phone">
+                    {/* <div class="list topbar d-flex justify-content-between">
+                        <div>9:41 AM</div>
+                        <div class="status-bar d-flex">
+                            <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/mobile-signal.svg"
+                                alt="signal"
+                            />
+                            <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/wifi.svg"
+                                alt="wifi"
+                            />
+                            <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/battery.svg"
+                                alt="battery"
+                            />
+                        </div>
+                    </div> */}
+                    <div class="list d-flex justify-content-between">
+                        <button class="btn btn-shadow btn-small" style={{ color: "#3271a8" }}>
+                            TV
+                        </button>
+
+                        <button class="btn btn-shadow">
+                            <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/microphone.svg"
+                                alt="microphone"
+                            />
+                        </button>
+                        <button class="btn btn-shadow btn-small no-mode">
+                            <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/power.svg"
+                                alt="undo"
+                            />
+                        </button>
+                    </div>
+                    <div class="list d-flex justify-content-between">
+                        <button class="btn  btn-shadow btn-small" style={{ color: "#3271a8" }}>
+                            Vol -
+                        </button>
+
+                        <button class="btn btn-shadow">
+                            <img src="/login.svg" alt="microphone" />
+                        </button>
+                        <button class="btn btn-shadow btn-small" style={{ color: "#3271a8" }}>
+                            Vol +
+                        </button>
+                    </div>
+                    <div class="list d-flex justify-content-between">
+                        <button class="btn">
+                            <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/backward.svg"
+                                alt="undo"
+                            />
+                        </button>
+                        <button class="btn">
+                            <img src="/play_pause.svg" alt="undo" />
+                        </button>
+                        <button class="btn">
+                            <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/forward.svg"
+                                alt="undo"
+                            />
+                        </button>
+                    </div>
+                    <div class="list d-flex justify-content-between">
+                        <button class="btn">
+                            <img src="/movie.svg" alt="undo" />
+                        </button>
+                        <button class="btn">
+                            Apps
+                            {/* <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/pause.svg"
+                                alt="undo"
+                            /> */}
+                        </button>
+                        <button class="btn">
+                            TV
+                            {/* <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/forward.svg"
+                                alt="undo"
+                            /> */}
+                        </button>
+                    </div>
+                    {/* Main Navigation */}
+                    <div class="list d-flex justify-content-between">
+                        <div class="side-btns">
+                            <div class="d-flex inner justify-content-between align-items-center">
+                                <button class="btn">
+                                    <img
+                                        src="https://www.yudiz.com/codepen/smart-remote-control/add.svg"
+                                        alt="add"
+                                    />
+                                </button>
+                                <div>Vol</div>
+                                <button class="btn">
+                                    <img
+                                        src="https://www.yudiz.com/codepen/smart-remote-control/minus.svg"
+                                        alt="minus"
+                                    />
+                                </button>
+                            </div>
+                        </div>
+                        <div class="center-btns">
+                            <button class="btn btn-up" onclick="handleKeyPress('up')">
+                                <img
+                                    src="https://www.yudiz.com/codepen/smart-remote-control/arrow-up.svg"
+                                    alt="arrow"
+                                />
+                            </button>
+                            <button class="btn btn-right" onclick="handleKeyPress('right')">
+                                <img
+                                    src="https://www.yudiz.com/codepen/smart-remote-control/arrow-right.svg"
+                                    alt="arrow"
+                                />
+                            </button>
+                            <button class="btn btn-ok" onclick="handleKeyPress('ok')">
+                                OK
+                            </button>
+                            <button class="btn btn-down" onclick="handleKeyPress('down')">
+                                <img
+                                    src="https://www.yudiz.com/codepen/smart-remote-control/arrow-down.svg"
+                                    alt="arrow"
+                                />
+                            </button>
+                            <button class="btn btn-left" onclick="handleKeyPress('left')">
+                                <img
+                                    src="https://www.yudiz.com/codepen/smart-remote-control/arrow-left.svg"
+                                    alt="arrow"
+                                />
+                            </button>
+                        </div>
+                        <div class="side-btns">
+                            <div class="d-flex inner justify-content-between align-items-center">
+                                <button class="btn">
+                                    <img
+                                        src="https://www.yudiz.com/codepen/smart-remote-control/arrow-up.svg"
+                                        alt="arrow"
+                                    />
+                                </button>
+                                <div>ch</div>
+                                <button class="btn">
+                                    <img
+                                        src="https://www.yudiz.com/codepen/smart-remote-control/arrow-down.svg"
+                                        alt="arrow"
+                                    />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="list d-flex justify-content-between">
+                        <button class="btn">
+                            <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/undo.svg"
+                                alt="undo"
+                            />
+                        </button>
+
+                        <button class="btn">
+                            <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/volume-cross.svg"
+                                alt="undo"
+                            />
+                        </button>
+                        <button class="btn">
+                            <img
+                                src="https://www.yudiz.com/codepen/smart-remote-control/home.svg"
+                                alt="home"
+                            />
+                        </button>
+                        <button class="btn">
+                            <img src="/search.svg" alt="undo" />
+                        </button>
+                    </div>
+
+                    <div class="list d-flex justify-content-between">
+                        <button class="btn">1</button>
+                        <button class="btn">2</button>
+                        <button class="btn">3</button>
+                    </div>
+                    <div class="list d-flex justify-content-between">
+                        <button class="btn">4</button>
+                        <button class="btn">5</button>
+                        <button class="btn">6</button>
+                    </div>
+                    <div class="list d-flex justify-content-between">
+                        <button class="btn">7</button>
+                        <button class="btn">8</button>
+                        <button class="btn">9</button>
+                    </div>
+                    <div class="list d-flex justify-content-between">
+                        <button class="btn">
+                            <img src="/clear.svg" alt="clear" />
+                        </button>
+                        <button class="btn">0</button>
+                        <button class="btn" style={{ color: "#3bd433" }}>
+                            -
+                        </button>
+                    </div>
+                    <div class="bottom-menu d-flex">
+                        {/* <button class="btn"> */}
+                        <img width={"80px"} src="/Univision_bosoo.png" alt="univision" />
+                        {/* </button> */}
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default Test;
